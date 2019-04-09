e928b6ee5d082841f70666b17026d3a76fdaef89
Constanza Lopez <constanza.vlm@hotmail.com>
2019-04-09 11:40:03 -0400

    template version 15

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ProductoInterface } from '../interfaces/producto.interface';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
+  
+  cargando= true;
+  productos: ProductoInterface[]= [];
 
   constructor( private http: HttpClient) { 
 
@@ -13,7 +19,7 @@ export class ProductosService {
   private cargarProductos(){
 
     this.http.get('https://angular-html-db4c9.firebaseio.com/productos_idx.json')
-      .subscribe( (resp: any[]) => {
+      .subscribe( (resp: ProductoInterface[]) => {
         console.log(resp);
       } );
   }
